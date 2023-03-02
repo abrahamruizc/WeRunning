4e5742bf77d214806e885304c376a265ae429ecd
alftacmir <alum.ataconm.2021@iesalixar.org>
2023-03-02 17:36:17 +0100

    cambios de module.module

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration/registration.component';
-import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { ListaEventosComponent } from './lista-eventos/lista-eventos.component';
 import { CoreModule } from '../core/core.module';
@@ -14,7 +13,6 @@ import { HistoricoEventosComponent } from './historico-eventos/historico-eventos
 @NgModule({
   declarations: [
     RegistrationComponent,
-    HomeComponent,
     ListaEventosComponent,
     UpdatepasswordComponent,
     HistoricoEventosComponent,
