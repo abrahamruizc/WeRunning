06c063360e106d476ad0d0e8352ddb397361c50f
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-03-02 17:24:13 +0100

    trabajando registro-evento

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { EventRegisterComponent } from './event-register/event-register.component';
 
 
 @NgModule({
   declarations: [
     RegistrationComponent,
-    HomeComponent
+    HomeComponent,
+    EventRegisterComponent
   ],
   imports: [
     CommonModule,
