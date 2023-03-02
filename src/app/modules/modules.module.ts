ea0bcc97d989e6a41d4264b794c610286ffba92c
Oscaralcala24 <alum.oalcalae.2020@iesalixar.org>
2023-03-02 17:17:15 +0100

    arreglos

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration/registration.component';
-import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
 import { ListaEventosComponent } from './lista-eventos/lista-eventos.component';
 import { CoreModule } from '../core/core.module';
 import { UpdatepasswordComponent } from './updatepassword/updatepassword.component';
-
 import { HistoricoEventosComponent } from './historico-eventos/historico-eventos.component';
 
 
@@ -14,7 +12,6 @@ import { HistoricoEventosComponent } from './historico-eventos/historico-eventos
 @NgModule({
   declarations: [
     RegistrationComponent,
-
     ListaEventosComponent,
     UpdatepasswordComponent,
     HistoricoEventosComponent,
