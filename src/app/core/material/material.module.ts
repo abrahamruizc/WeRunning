a3f38a5fe3304057e54f1c67b25441965333b070
Oscaralcala24 <alum.oalcalae.2020@iesalixar.org>
2023-03-03 17:37:17 +0100

    modificaciones

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -12,6 +12,7 @@ import {MatInputModule} from '@angular/material/input';
 
 
 
+
 @NgModule({
   declarations: [],
   imports: [
--- src/app/modules/historico-eventos/historico-eventos.component.ts
+++ src/app/modules/historico-eventos/historico-eventos.component.ts
@@ -7,9 +7,9 @@ import { Evento } from '../evento/evento.model'
   styleUrls: ['./historico-eventos.component.scss']
 })
 export class HistoricoEventosComponent {
-  model = new Evento(1,"Carrera nocturna","Sevilla",9,new Date(23-10-2023),"../../assets/img/","ojfjodsjlfñjdslfjlsdjflsjfljdslfjdsljflsmj´fojlfkdsjlfksdjsdl");
-  model1 = new Evento(1,"Carrera nocturna","Sevilla",9,new Date(23-10-2023),"../../assets/img/","ojfjodsjlfñjdslfjlsdjflsjfljdslfjdsljflsmj´fojlfkdsjlfksdjsdl");
-  model2 = new Evento(1,"Carrera nocturna","Sevilla",9,new Date(23-10-2023),"../../assets/img/","ojfjodsjlfñjdslfjlsdjflsjfljdslfjdsljflsmj´fojlfkdsjlfksdjsdl");
-  model3 = new Evento(1,"Carrera nocturna","Sevilla",9,new Date(23-10-2023),"../../assets/img/","ojfjodsjlfñjdslfjlsdjflsjfljdslfjdsljflsmj´fojlfkdsjlfksdjsdl");
+  model = new Evento(1,"Carrera nocturna","Sevilla",9,new Date("2022-02-03T21:00:00.000Z"),"../../assets/img/Cartel_Nocturna_2022.jpg","ojfjodsjlfñjdslfjlsdjflsjfljdslfjdsljflsmj´fojlfkdsjlfksdjsdl");
+  model1 = new Evento(2,"Carrera popular constantina","Sevilla",15,new Date("2021-04-03T17:00:00.000Z"),"../../assets/img/","ojfjodsjlfñjdslfjlsdjflsjfljdslfjdsljflsmj´fojlfkdsjlfksdjsdl");
+  model2 = new Evento(3,"Carrera nocturna","Sevilla",9,new Date("2022-05-21T16:00:00.000Z"),"../../assets/img/","ojfjodsjlfñjdslfjlsdjflsjfljdslfjdsljflsmj´fojlfkdsjlfksdjsdl");
+  model3 = new Evento(4,"Carrera nocturna","Sevilla",9,new Date("2023-07-03T21:30:00.000Z"),"../../assets/img/","ojfjodsjlfñjdslfjlsdjflsjfljdslfjdsljflsmj´fojlfkdsjlfksdjsdl");
   eventos=[this.model,this.model1,this.model2,this.model3];
 }
