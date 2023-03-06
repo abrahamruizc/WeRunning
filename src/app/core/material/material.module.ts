6fb280ec3295064857b3c14d161e6859869dc5c4
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-03-06 17:26:16 +0100

    angular funciones

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -7,7 +7,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from "@angular/material/card";
-
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 
@@ -26,7 +26,8 @@ import { MatCardModule } from "@angular/material/card";
     MatCardModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatCheckboxModule
   ],
   exports: [
     MatSlideToggleModule,
@@ -38,7 +39,8 @@ import { MatCardModule } from "@angular/material/card";
     MatCardModule,
     MatCardModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatCheckboxModule
   ]
 })
 export class MaterialModule { }
--- src/app/modules/event-register/event-register.component.ts
+++ src/app/modules/event-register/event-register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-event-register',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./event-register.component.scss']
 })
 export class EventRegisterComponent {
+  
+
+nombre = new FormControl('', [Validators.required, Validators.minLength(8)]);
+apellidos = new FormControl('', [Validators.required, Validators.minLength(8)]);
+correo = new FormControl('', [Validators.required, Validators.email]);
+fecha = new FormControl('', [Validators.required]);
+
+submitted = false;
+
+
+onSubmit() { 
+  this.submitted = true;
+  
+}
+
+
+getErrorMessage() {
+  if (this.nombre.hasError('required')) {
+    return 'Debes introducir un nombre';
+  } 
+  else if(this.apellidos.hasError('required')){
+    return 'Debes introducir un apellido';
+  } 
+  else if(this.correo.hasError('required')){
+    return 'Debes introducir un correo electronico';
+  }
+  else if(this.fecha.hasError('required')){
+    return 'Debes introducir un fecha';
+  }
+
+  return this.nombre.hasError('email') ? 'Not a valid email' : '';
+}
 
 }
