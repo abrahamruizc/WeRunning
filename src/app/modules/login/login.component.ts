d3a84d2d1a3bf893c3f0e3879aa15a17ef172d5b
alumjruizr <alum.jruizr.2021@iesalixar.org>
2023-03-07 15:03:44 +0100

    commit validaciones

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,18 +1,34 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { User } from './user.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
+
+
 export class LoginComponent {
   signin : FormGroup = new FormGroup({
-    email: new FormControl('', [Validators.email, Validators.required ]),
+    username: new FormControl('', [Validators.required, Validators.required ]),
     password: new FormControl('', [Validators.required, Validators.min(3) ])
   });
   hide = true;
-  get emailInput() { return this.signin.get('email'); }
+
+  model = new User("admin", "admin");
+
+  submitted = false;
+  onSubmit() {this.submitted = true;}
+
+  get emailInput() { return this.signin.get('username'); }
   get passwordInput() { return this.signin.get('password'); } 
  
 }
+
+//function getErrorMessagenombre() {
+//  if(this.signin.email.hasError('required')){
+//   return 'Debes introducir un nombre';
+//  } 
+//  return this.password.hasError('password') ? 'No es un nombre valido' : '';
+//}
