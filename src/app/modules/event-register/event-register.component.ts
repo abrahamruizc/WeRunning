d579c8c05cb52ec7e886194d0cb24955a0c4175b
abrahamruizc <alum.aruizc.2020@iesalixar.org>
2023-03-07 13:56:10 +0100

    form modal

--- src/app/modules/event-register/event-register.component.ts
+++ src/app/modules/event-register/event-register.component.ts
@@ -13,31 +13,72 @@ nombre = new FormControl('', [Validators.required, Validators.minLength(8)]);
 apellidos = new FormControl('', [Validators.required, Validators.minLength(8)]);
 correo = new FormControl('', [Validators.required, Validators.email]);
 fecha = new FormControl('', [Validators.required]);
+contrasena1 = new FormControl('', [Validators.required]);
+contrasena2 = new FormControl('', [Validators.required]);
 
 submitted = false;
 
-
+//, Validators.pattern("^(?=^[ -~]{6,64}$)(?=.([a-z][A-Z]))(?=.[0-9])(.*[ -/|:-@|[-`|{-~]).+$")
 onSubmit() { 
   this.submitted = true;
   
 }
 
 
-getErrorMessage() {
-  if (this.nombre.hasError('required')) {
+getErrorMessagenombre() {
+  if(this.apellidos.hasError('required')){
     return 'Debes introducir un nombre';
   } 
-  else if(this.apellidos.hasError('required')){
+ 
+
+  return this.nombre.hasError('nombre') ? 'No es un nombre valido' : '';
+}
+
+
+getErrorMessageapellidos() {
+  if(this.apellidos.hasError('required')){
     return 'Debes introducir un apellido';
   } 
-  else if(this.correo.hasError('required')){
+ 
+
+  return this.apellidos.hasError('apellido') ? 'No es un apellido valido' : '';
+}
+
+
+getErrorMessagecorreo() {
+  if(this.correo.hasError('required')){
     return 'Debes introducir un correo electronico';
   }
-  else if(this.fecha.hasError('required')){
+
+  return this.correo.hasError('correo') ? 'No es un correo valido' : '';
+}
+
+getErrorMessagefecha() {
+  if(this.fecha.hasError('required')){
     return 'Debes introducir un fecha';
   }
 
-  return this.nombre.hasError('email') ? 'Not a valid email' : '';
+  return this.fecha.hasError('fecha') ? 'No es una fecha valido' : '';
+}
+
+
+getErrorMessagecontra1() {
+  if(this.contrasena1.hasError('required')){
+    return 'Debes introducir una contraseña';
+  } 
+
+  return this.contrasena1.hasError('contrasena') ? 'No es una contraseña valida' : '';
+}
+
+getErrorMessagecontra2() {
+  if(this.contrasena2.hasError('required')){
+    return 'Debes introducir una contraseña';
+  } 
+  if(this.contrasena1 != this.contrasena2){
+    return 'Las contraseñas deben coincidir';
+  }
+
+  return this.contrasena2.hasError('contrasena') ? 'No es una contraseña valida' : '';
 }
 
 }
